81c567f1443a77bc0121514887bb6e5a06d1ee0d
Guilherme Henrique da Silva <g281217@dac.unicamp.br>
2025-08-31 22:34:32 -0300

    feat: main header added

---
+++ src/app/(pages)/beta/teste/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Header from "@/components/header/header"
+
+
+export default function Teste(){
+  return (
+    <div>
+      <Header variant="guest"></Header>
+      <h1>Teste</h1>
+    </div>
+  )
+}
---
+++ src/components/header/header.tsx
@@ -0,0 +1,242 @@
+import { Button } from "@/components/ui/button"
+import {CircleUserRound, Settings, LogOut, Menu, Heart, Calendar, Home, Inbox, Search, LogIn, UserRound} from "lucide-react"
+import { useRouter } from "next/navigation";
+import { useState } from 'react';
+
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+
+type Variant = 'guest' | 'customer' | 'agent' | 'admin';
+
+type NavItem = {
+  label: string;
+  href: string;
+};
+
+type SidebarItem = {
+  label: string;
+  href: string;
+  icon: React.ElementType;
+};
+
+
+
+const navConfig: Record<Variant, NavItem[]> = {
+  guest: [
+    { label: 'Buscar Imóveis', href: '/' },
+    { label: 'Sou corretor(a)', href: '/' },
+  ],
+  customer: [
+    { label: 'Buscar Imóveis', href: '/' },
+    { label: 'Visitas', href: '/' },
+    { label: 'Reservas', href: '/' },
+  ],
+  agent: [
+    { label: 'Buscar Imóveis', href: '/' },
+    { label: 'Visitas', href: '/' },
+    { label: 'Reservas', href: '/' },
+  ],
+  admin: [
+    { label: 'Buscar Imóveis', href: '/' },
+    { label: 'Visitas', href: '/' },
+    { label: 'Reservas', href: '/' },
+    { label: 'Editar Imóveis', href: '/' },
+    { label: 'Contas', href: '/' },
+  ],
+};
+
+const profileSidebar: Record<Variant, SidebarItem[]> = {
+  guest: [
+    { label: 'Entrar', href: '/', icon: LogIn },
+  ],
+  customer: [
+    { label: 'Configurações', href: '/', icon: Settings },
+    { label: 'Favoritos', href: '/', icon: Heart },
+    { label: 'Sair', href: '/', icon: LogOut },
+  ],
+  agent: [
+    { label: 'Configurações', href: '/', icon: Settings },
+    { label: 'Sair', href: '/', icon: LogOut },
+  ],
+  admin: [
+    { label: 'Configurações', href: '/', icon: Settings },
+    { label: 'Sair', href: '/', icon: LogOut },
+  ],
+}
+
+const menuSidebar: Record<Variant, SidebarItem[]> = {
+  guest: [
+    { label: 'Buscar Imóveis', href: '/', icon: Home },
+    { label: 'Sou corretor(a)', href: '/', icon: Search },
+  ],
+  customer: [
+    { label: 'Buscar Imóveis', href: '/', icon: Home },
+    { label: 'Visitas', href: '/', icon: Calendar },
+    { label: 'Reservas', href: '/', icon: Inbox },
+  ],
+  agent: [
+    { label: 'Buscar Imóveis', href: '/', icon: Home },
+    { label: 'Visitas', href: '/', icon: Calendar },
+    { label: 'Reservas', href: '/', icon: Inbox },
+  ],
+  admin: [
+    { label: 'Buscar Imóveis', href: '/', icon: Home },
+    { label: 'Visitas', href: '/', icon: Calendar },
+    { label: 'Reservas', href: '/', icon: Inbox },
+    { label: 'Editar Imóveis', href: '/', icon: Settings },
+    { label: 'Contas', href: '/', icon: UserRound },
+  ],
+}
+
+interface HeaderProps {
+  variant: Variant;
+}
+
+
+function Header({variant}: HeaderProps) {
+    const links = navConfig[variant];
+    const router = useRouter();
+
+
+  return (
+    <SidebarProvider className="h-[80px] bg-green-200">
+
+      <Sidebar>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel>Meu Perfil</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {profileSidebar[variant].map((item) => (
+                  <SidebarMenuItem key={item.label}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.href}>
+                        <item.icon />
+                        <span>{item.label}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+            <SidebarGroupLabel>Menu</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {menuSidebar[variant].map((item) => (
+                  <SidebarMenuItem key={item.label}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.href}>
+                        <item.icon />
+                        <span>{item.label}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+
+      <header className="w-full h-[80px] py-4 px-6 bg-[#332475] flex justify-start gap-1.5 lg:gap-0 lg:justify-between items-center">
+
+        <SidebarTrigger className="lg:hidden text-white" />
+
+        <div>
+            <h1 className="text-3xl text-white select-none w-[200px]">Meu Apê</h1>
+        </div>
+
+        <div className="justify-end items-center w-full gap-10 hidden lg:flex">
+            <div className="flex gap-6">
+                {links.map((link) => (
+                    <a 
+                        key={link.label}
+                        className="text-white cursor-pointer font-bold relative after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-500 hover:after:w-full"
+                        onClick={() => router.push(link.href)}>
+                        {link.label}
+                    </a>
+                ))}
+            </div>
+
+            <div>
+              {variant === 'guest'? 
+              (
+                <Button
+                    variant="outline"
+                    className="cursor-pointer"
+                    onClick={() => router.push('/')} // Navigate to login page
+                >
+                    <CircleUserRound className="mr-1" />
+                    <span className="font-semibold">Entrar</span>
+                </Button>
+
+              ) : ( 
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="outline">
+                      <CircleUserRound className="mr-1" />
+                      <span className="font-semibold">Conta</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-56 mr-4" >
+                    <DropdownMenuLabel>Meu Perfil</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuCheckboxItem
+                      onClick={() => router.push('/')}
+                    >
+                      <Settings className="mr-1" />
+                      Configurações
+                    </DropdownMenuCheckboxItem>
+                    {
+                      variant === 'customer' && (
+                        <DropdownMenuCheckboxItem
+                          onClick={() => router.push('/')}
+                        >
+                          <Heart className="mr-1" />
+                          Favoritos
+                        </DropdownMenuCheckboxItem>
+                      )
+                    }
+                    
+                    <DropdownMenuCheckboxItem
+                      onClick={() => router.push('/')}
+                    >
+                      <LogOut className="mr-1 text-red-500" />
+                      <span className="text-red-500">Sair</span>
+                    </DropdownMenuCheckboxItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                
+
+              )}
+            </div>
+        </div>
+
+      </header>
+    </SidebarProvider>
+    
+  )
+}
+
+export default Header
+
