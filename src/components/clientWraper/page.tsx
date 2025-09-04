935d4f81ad268ee1b8dbc2dc681c0be7ca78853f
Guilherme Henrique da Silva <guilherme.silva@conpec.com.br>
2025-09-03 21:47:12 -0300

    feat: wrap pages with header and footer layout

---
+++ src/components/clientWraper/page.tsx
@@ -0,0 +1,21 @@
+// components/ClientWrapper.tsx
+'use client';
+
+import { useState } from 'react';
+import { SidebarProvider } from '@/components/ui/sidebar';
+import Header from '@/components/header/header';
+import Footer from '@/components/footer/footer';
+import { AppSidebar } from '@/components/header/app-sidebar';
+
+export default function ClientWrapper({ children }: { children: React.ReactNode }) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <SidebarProvider open={open} onOpenChange={setOpen} className='min-h-screen flex flex-col'>
+        <AppSidebar variant="admin" />
+        <Header variant="admin" />
+        <main className='flex-grow'>{children}</main>
+        <Footer />
+    </SidebarProvider>
+  );
+}
