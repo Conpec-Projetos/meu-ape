b4ca246c8db4c191c049d9ca31586b662540a81e
Jeorde <jeorde.neto@conpec.com.br>
2025-09-10 14:15:50 -0300

    Card de propriedades aprimorado, função de scroll criada. -TODO: Custom scroll bar, embed do maps, header, sistema funcional de filtros

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -25,6 +25,7 @@ function DropdownMenuTrigger({
 }: React.ComponentProps<typeof DropdownMenuPrimitive.Trigger>) {
   return (
     <DropdownMenuPrimitive.Trigger
+      className="rounded-full"
       data-slot="dropdown-menu-trigger"
       {...props}
     />
