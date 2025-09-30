2c87215860d9876d87d5f1ae8d730506f357e63c
Jeorde <jeorde.neto@conpec.com.br>
2025-09-30 10:00:43 -0300

    feat: new user config screen design

--- src/app/(pages)/beta/user-config/page.tsx
+++ src/app/(pages)/beta/user-config/page.tsx
@@ -1,11 +1,10 @@
 "use client"
-
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import React from "react";
 import { PasswordModal } from "@/components/passwordModal/passwordModal";
 import {
-        InputModal,
+        Dialog,
         DialogClose,
         DialogContent,
         DialogDescription,
@@ -15,7 +14,7 @@ import {
         DialogPortal,
         DialogTitle,
         DialogTrigger,
-} from "@/components/ui/user-config/InputModal";
+} from "@/components/ui/dialog";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { DownloadIcon } from "lucide-react";
 
@@ -25,18 +24,18 @@ export default function UserConfig() {
 
         return (
 
-                <main className="flex flex-col h-max">
-                        <div className="flex flex-col pt-1">
+                <main className="flex-grow bg-gray-50">
+                        <div className="flex flex-col pt-15">
 
-                                <div>
+                                <div className="">
                                         <Button
-                                                className={`cursor-pointer w-[120px] h-[29px] rounded-t-[8px] rounded-b-none shadow-[0px_4px_3px_rgba(0,0,0,0.45)] ${activeTab === "conta" ? "bg-[#DEDEDE] text-black" : "bg-gray-200 text-gray-500"}`}
+                                                className={`rounded-b-none cursor-pointer w-[120px] h-[29px] ${activeTab === "conta" ? "rounded-t-[8px]  shadow-[0px_4px_3px_rgba(0,0,0,0.45)] bg-[#DEDEDE] text-black" : "bg-background text-gray-500"}`}
                                                 onClick={() => setActiveTab("conta")}
                                         >
                                                 Conta
                                         </Button>
                                         <Button
-                                                className={`cursor-pointer w-[120px] h-[29px] rounded-t-[8px] rounded-b-none shadow-[0px_4px_3px_rgba(0,0,0,0.45)] ${activeTab === "documentos" ? "bg-[#DEDEDE] text-black" : "bg-gray-200 text-gray-500"}`}
+                                                className={`rounded-b-none cursor-pointer w-[120px] h-[29px] ${activeTab === "documentos" ? "rounded-t-[8px]  shadow-[0px_4px_3px_rgba(0,0,0,0.45)] bg-[#DEDEDE] text-black" : "bg-background text-gray-500"}`}
                                                 onClick={() => setActiveTab("documentos")}
                                         >
                                                 Documentos
@@ -46,38 +45,38 @@ export default function UserConfig() {
 
                         </div>
 
-                        <div className="flex flex-col justify-center items-center pt-1.5">
-                                        {activeTab === "conta" && (
-                                                <>
-                                <div className="h-[567px] w-[510px] bg-[#D9D9D9] rounded-[20px] shadow-[0px_4px_3px_rgba(0,0,0,0.45)] ">
+                        <div className="flex flex-col justify-center items-center pt-2">
+                                {activeTab === "conta" && (
+                                        <>
+                                                <div className="">
                                                         <div className="pl-15 pt-6">
                                                                 <div className="flex flex-col items-start pt-2">
-                                                                        <h1 className="text-[#4D4D4D] text-[15px]">Nome Completo</h1>
-                                                                        <Input className=" pl-5 rounded-[10px] bg-white w-[390px] h-[43px]"></Input>
+                                                                        <h1 className="text-[#4D4D4D] text-[12px]">Nome Completo</h1>
+                                                                        <Input className=" border-1 border-gray-400 pl-5 rounded-[10px] bg-white w-[340px] h-[43px]"></Input>
                                                                 </div>
                                                                 <div className="flex flex-col items-start pt-4">
-                                                                        <h1 className="text-[#4D4D4D] text-[15px]">Email</h1>
-                                                                        <Input className="pl-5 rounded-[10px] bg-white w-[390px] h-[43px] "></Input>
+                                                                        <h1 className="text-[#4D4D4D] text-[12px]">Email</h1>
+                                                                        <Input className=" border-1 border-gray-400 pl-5 rounded-[10px] bg-white w-[340px] h-[43px]"></Input>
                                                                 </div>
                                                                 <div className="flex flex-col items-start pt-4">
-                                                                        <h1 className="text-[#4D4D4D] text-[15px]">Endereço</h1>
-                                                                        <Input className="pl-5 rounded-[10px] bg-white w-[390px] h-[43px] "></Input>
+                                                                        <h1 className="text-[#4D4D4D] text-[12px]">Endereço</h1>
+                                                                        <Input className=" border-1 border-gray-400 pl-5 rounded-[10px] bg-white w-[340px] h-[43px]"></Input>
                                                                 </div>
                                                                 <div className="flex flex-col items-start pt-4">
-                                                                        <h1 className="text-[#4D4D4D] text-[15px]">CPF</h1>
-                                                                        <Input className="pl-5 rounded-[10px] bg-white w-[390px] h-[43px] "></Input>
+                                                                        <h1 className="text-[#4D4D4D] text-[12px]">CPF</h1>
+                                                                        <Input className=" border-1 border-gray-400 pl-5 rounded-[10px] bg-white w-[340px] h-[43px]"></Input>
                                                                 </div>
                                                                 <div className="flex flex-col items-start pt-4">
-                                                                        <h1 className="text-[#4D4D4D] text-[15px]">Telefone</h1>
-                                                                        <Input className="pl-5 rounded-[10px] bg-white w-[390px] h-[43px] "></Input>
+                                                                        <h1 className="text-[#4D4D4D] text-[12px]">Telefone</h1>
+                                                                        <Input className=" border-1 border-gray-400 pl-5 rounded-[10px] bg-white w-[340px] h-[43px]"></Input>
                                                                 </div>
                                                         </div>
                                                         <div className="pl-15 pt-5">
-                                                                <InputModal>
+                                                                <Dialog>
                                                                         <VisuallyHidden>
                                                                                 <DialogTitle>Alterar Senha</DialogTitle>
                                                                         </VisuallyHidden>
-                                                                        <DialogTrigger>Editar Senha</DialogTrigger>
+                                                                        <DialogTrigger className="underline cursor-pointer">Alterar Senha</DialogTrigger>
                                                                         <DialogOverlay></DialogOverlay>
                                                                         <DialogContent className="fixed top-1/2 left-1/2 z-50 w-[468px] h-[401px] max-w-md -translate-x-[51%] -translate-y-2/3 rounded-xl bg-[#B0B0B0] p-6 shadow-[0px_5px_2px_rgba(0,0,0,0.45)]">
                                                                                 <div className="mt-4 flex flex-col gap-y-4 items-center">
@@ -98,34 +97,50 @@ export default function UserConfig() {
                                                                                         </div>
                                                                                 </div>
                                                                         </DialogContent>
-                                                                </InputModal>
+                                                                </Dialog>
                                                         </div>
-                                                        <div className="flex justify-evenly pt-8">
-                                                                <Button className="bg-[#6049C7] w-[100px] hover:bg-[#4f39a8] cursor-pointer">Excluir Conta</Button>
-                                                                <Button className="bg-[#6049C7] w-[100px] hover:bg-[#4f39a8] cursor-pointer">Salvar</Button>
+                                                        <div className="pl-15 flex items-center justify-start flex-col">
+                                                                <Button className="mt-2 mb-3 bg-black w-[340px] h-[50px] hover:bg-blend-color-burn cursor-pointer rounded-full">Salvar</Button>
+                                                                <Button className="bg-red-400 text-red-900 w-[340px] h-[50px] border-2 hover:bg-red-500 border-red-00 cursor-pointer rounded-full">Excluir Conta</Button>
                                                         </div>
-                                </div>
-                                                </>
-                                        )}
-                                        {activeTab === "documentos" && (
-                                                <div className="flex flex-col gap-5 pl-20 items-start justify-center h-full w-full">
-                                                        <h1 className="text-2xl text-[#4D4D4D]">RG ou CIN:</h1>
-                                                        <DownloadIcon className="ml-10 text-[#4D4D4D] border-[#4D4D4D] border-3 border-dashed rounded-[10px] h-25 w-25"></DownloadIcon>
-                                                        <h1 className="text-2xl text-[#4D4D4D]">Comprovante de endereço:</h1>
-                                                        <DownloadIcon className="ml-10 text-[#4D4D4D] border-[#4D4D4D] border-3 border-dashed rounded-[10px] h-25 w-25"></DownloadIcon>
+                                                </div>
+                                        </>
+                                )}
+                                {activeTab === "documentos" && (
+                                        <div className="flex flex-col gap-5 pl-20 items-start justify-center h-full w-full">
+                                                <div className="relative">
+                                                        <h1 className="pb-1 text-2xl text-[#4D4D4D]">RG ou CIN:</h1>
+                                                        <DownloadIcon className="ml-10 p-6 pt-1 text-gray-400 border-gray-400 border-3 border-dashed rounded-[10px] h-27 w-27" />
+                                                        <h2 className="absolute text-gray-600 text-[12px] bottom-[14px] right-6">RG ou CIN</h2>
+                                                </div>
+                                                <div className="relative">
+                                                        <h1 className="pb-1 text-2xl text-[#4D4D4D]">Comprovante de endereço:</h1>
+                                                        <DownloadIcon className="ml-10 p-6 pt-1 text-gray-400 border-gray-400 border-3 border-dashed rounded-[10px] h-27 w-27" />
+                                                        <h2 className="absolute text-gray-600 text-[12px] bottom-[5px] left-[50px]">Comprovante de<span className="flex justify-center">endereço</span></h2>
+                                                </div>
 
-                                                        <h1 className="text-2xl text-[#4D4D4D]">Comprovante de renda:</h1>
-                                                        <DownloadIcon className="ml-10 text-[#4D4D4D] border-[#4D4D4D] border-3 border-dashed rounded-[10px] h-25 w-25"></DownloadIcon>
-
-                                                        <h1 className="text-2xl text-[#4D4D4D]">Certidão de casamento:</h1>
-                                                        <DownloadIcon className="ml-10 text-[#4D4D4D] border-[#4D4D4D] border-3 border-dashed rounded-[10px] h-25 w-25"></DownloadIcon>
-                                                        <Button className="bg-[#6049C7] w-[100px] hover:bg-[#4f39a8] cursor-pointer flex justify-center items-center">Salvar</Button>
-
-
-
+                                                <div className="relative">
+                                                        <h1 className="pb-1 text-2xl text-[#4D4D4D]">Comprovante de renda:</h1>
+                                                        <DownloadIcon className="ml-10 p-6 pt-1 text-gray-400 border-gray-400 border-3 border-dashed rounded-[10px] h-27 w-27" />
+                                                        <h2 className="absolute text-gray-600 text-[12px] bottom-[5px] left-[50px]">Comprovante de <span className="flex justify-center">renda</span></h2>
 
                                                 </div>
-                                        )}
+                                                <div className="relative ">
+                                                        <h1 className="pb-1 text-2xl text-[#4D4D4D]">Certidão de casamento:</h1>
+                                                        <DownloadIcon className="ml-10 p-6 pt-1 text-gray-400 border-gray-400 border-3 border-dashed rounded-[10px] h-27 w-27" />
+                                                        <h2 className="absolute text-gray-600 text-[12px] bottom-[5px] left-[65px]">Certidão de<span className="flex justify-center">casamento</span></h2>
+
+                                                </div>
+                                                <div className="flex items-center justify-center w-screen h-auto pb-2">
+                                                <Button className="bg-black w-[100px] hover:bg-blend-color-burn cursor-pointer ">Salvar</Button>
+
+                                                </div>
+
+
+
+
+                                        </div>
+                                )}
                         </div>
 
                 </main>
