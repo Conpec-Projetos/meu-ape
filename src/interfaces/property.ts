2f6c348dd5e652299b9bbd279c96d8bea666c122
LeonardoF-Conpec <leonardo.franco@conpec.com.br>
2025-10-03 18:11:22 -0300

    docs: updated GEMINI.md file and added comments to the interfaces

--- src/interfaces/property.ts
+++ src/interfaces/property.ts
@@ -13,23 +13,24 @@ interface SearchableUnitFeats {
 }
 
 export interface Property {
-    id?: string;
-    developerRef: DocumentReference; // ou string
+    id?: string; // id do documento no Firebase
+    developerRef: DocumentReference; // referência ao documento da construtora na coleção developers
     name: string;
-    address: string;
-    location: GeoPoint;
-    deliveryDate: Date | Timestamp;
+    address: string; // endereço em linguagem natural
+    location: GeoPoint; // coordenadas
+    deliveryDate: Date | Timestamp; 
     launchDate: Date | Timestamp;
-    features: string[];
-    floors: number;
-    unitsPerFloor: number;
-    availableUnits?: number;
-    images?: string[];
-    description: string;
-    searchableUnitFeats: SearchableUnitFeats;
-    availableUnitsCount: number;
-    groups: string[];
-    units?: Unit[]; // Representação para a subcoleção
+    features: string[]; // lista de caracteríscas do empreendimento, ex: "academia", "quadra de esportes"
+    floors: number; // número de pavimentos
+    unitsPerFloor: number; // números de unidades por pavimento/andar
+    availableUnits?: number; // quantidade de unidades disponíveis
+    propertyImages?: string[]; // URLs das imagens do empreendimento no Cloud Storage
+    areasImages?: string[]; // URLs das imagens das áreas de convívio no Cloud Storage
+    matterportUrl?: string[] // URLs para os scans 3D do Matterport (pode ser mais de um)
+    description: string; // descrição do imóvel)
+    searchableUnitFeats: SearchableUnitFeats; // campo desnormalizado para otimizar a busca
+    groups: string[]; // array de grupos de corretores que podem visualizar aquele imóvel
+    units?: Unit[]; // representação para a subcoleção
     createdAt: Date | Timestamp;
     updatedAt: Date | Timestamp;
 }
